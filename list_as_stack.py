fa5aba824477503bfde5cb7e63adb4c417e81c28
Venkatesh Kumar Vangala <venkatesh.vangala@imaginea.com>
2017-05-17 20:14:41 +0530

    example to use list as stack

---
+++ list_as_stack.py
@@ -0,0 +1,14 @@
+''' List as a Stack '''
+
+stack = [1,2, 3, 4]
+stack.append(5)
+stack.append(6)
+print("stack after push: ", stack)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print("poped element:", stack.pop())
+print("stack after pop:", stack)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print("poped element:", stack.pop())
+print("stack after pop:", stack)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(stack)
