c4900357a3fdfb1b7cf52c66893038044703be00
Venkatesh Kumar Vangala <venkatesh.vangala@imaginea.com>
2017-05-17 16:20:11 +0530

    unpacking args

---
+++ unpack_args.py
@@ -0,0 +1,8 @@
+'''dictionaries can deliver keyword arguments with the **-operator:'''
+def parrot(voltage, state='a stiff', action='voom'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ')
+    print("E's", state, "!")
+
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+parrot(**d)
