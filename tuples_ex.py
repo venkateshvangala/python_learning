0ff245f0d1b898838f385b7265c831ac42509594
Venkatesh Kumar Vangala <venkatesh.vangala@imaginea.com>
2017-05-18 12:41:12 +0530

    tuples example in all scenarios

---
+++ tuples_ex.py
@@ -0,0 +1,20 @@
+# Tuples are immutable soo assignment is not possible t[0] = 111
+# empty tuple creation example
+empty = ()
+print("empty tuple is: ", empty)
+print("lenth of empty tuple is: ", len(empty))
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# single tuple creation example (single element with comma precedance)
+
+singleton = 'hello', 
+print("single tuple is: ", singleton)
+print("lenth of singleton tuple is: ", len(singleton))
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# multi element tuple example
+touple = 1, 2, 3, 'helloo', 'world', 'example tuple' # touple = (1, 2, 3, 'helloo', 'world', 'example tuple')
+print("tuple elements are: ", touple)
+print("tuple lenght is: ", len(touple))
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
